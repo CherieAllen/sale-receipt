ba9958cb388fcc3d684cbce194ac467bd006c1ff
Cherie Allen <cherie@Cheries-MacBook-Air.local>
2022-04-22 12:08:56 -0400

    Currency

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-const subtotal = 19.75;
-const taxRate = 0.07;
-
-function calculateTax(subtotal, taxRate) {
-    const tax = subtotal * taxRate;
-    return tax;
-}
-
-const myTax = calculateTax(subtotal, taxRate);
-const myTotal = calculateTotal(subtotal, myTax);
-
-function calculateTotal(subtotal, tax){
-    return subtotal + tax;
-}
+// const subtotal = 19.75;
+// const taxRate = 0.07;
+// 
+// function calculateTax(subtotal, taxRate) {
+    // const tax = subtotal * taxRate;
+    // return tax;
+// }
 
 
+// const myTax = calculateTax(subtotal, taxRate);
+// const myTotal = calculateTotal(subtotal, myTax);
+// 
+// function calculateTotal(subtotal, tax){
+    // return subtotal + tax;
+// }
 
-console.log('SubTotal:       ', subtotal.toFixed(2));
-console.log('Tax:         ', myTax.toFixed(2));
-console.log('---------------------');
-console.log('TOTAL:', myTotal.toFixed(2));
+
+
+
+// console.log('SubTotal:       ', subtotal.toFixed(2));
+// console.log('Tax:         ', myTax.toFixed(2));
+// console.log('---------------------');
+// console.log('TOTAL:', myTotal.toFixed(2));
+
+
+
+// const usd = 19.75;
+// const pounds = 0.78;
+// 
+// function convertCurrency (curency,conversionRate){
+        // const curreturn = curency * conversionRate;
+        // return curreturn;
+// }
+// console.log (convertCurrency(usd,pounds));
+// const result = convertCurrency(usd,pounds)
+// 
+// const name = 'zhuopu'
+// console.log(`${usd.toFixed(2)} in USD is ${result.toFixed(2)} pounds`)
+
+
+
+const 
